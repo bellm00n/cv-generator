628f3f4791fc2ba0fc005f8b89c9e97e6d303afc
Igor Kombeev <Igor_Kombeev@epam.com>
2026-03-10 10:39:30 +0100

    feat: allow to open landing without the authorization

--- middleware.ts
+++ middleware.ts
@@ -11,5 +11,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|fonts).*)"],
+  matcher: ["/cv/:path*", "/cv-list/:path*"],
 };
